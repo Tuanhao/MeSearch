acb01a00db81e2207b466d5751dcd3146b917a82
Tuanhao <tuanhao256@gmail.com>
2020-02-12 15:35:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+
+  // Generate some passwords
+  const passwords = [1,2,3,4,5]
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log('Sent passwords');
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
