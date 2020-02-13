635745c0ad1905612e539cebb77e1ff8be383998
Tuanhao <tuanhao256@gmail.com>
2020-02-13 15:17:48 -0500

    adding React client folder

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // Initialize state
+  state = { passwords: [] }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
+
+  render() {
+    const { passwords } = this.state;
+
+    return (
+      <div className="App">
+      {passwords}
+      </div>
+    )
+  }
+}
+
+export default App;
