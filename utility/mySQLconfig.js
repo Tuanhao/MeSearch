44d2b9161d28255cfadd5cb4dd2640495f1de816
Tuanhao <tuanhao256@gmail.com>
2020-02-21 17:06:35 -0500

    update mySQL credentials

--- utility/mySQLconfig.js
+++ utility/mySQLconfig.js
@@ -1,8 +1,16 @@
 let config = {
-  host: 'localhost',
-  user: 'root',
-  password: 'mesearch123',
-  database: 'mesearch-general'
+  host: 'http://tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/',
+  user: 'hho5brfpbmc6ofnj',
+  password: 'fh6u94dctasut574',
+  database: 'mesearch-nrs3xu9ruvw517q0'
 }
+
+// local config
+// let config = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'mesearch123',
+//   database: 'mesearch-general'
+// }
 
 module.exports = config
