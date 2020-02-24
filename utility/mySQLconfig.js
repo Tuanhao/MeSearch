405b513bbb60ddb2750283f5242b7910894666f6
Tuanhao <tuanhao256@gmail.com>
2020-02-24 15:17:26 -0500

    fix minor issue with package.json script and DB credentials

--- utility/mySQLconfig.js
+++ utility/mySQLconfig.js
@@ -1,8 +1,8 @@
 let config = {
-  host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/',
+  host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'hho5brfpbmc6ofnj',
   password: 'fh6u94dctasut574',
-  database: 'mesearch-nrs3xu9ruvw517q0'
+  database: 'nrs3xu9ruvw517q0'
 }
 
 // local config
