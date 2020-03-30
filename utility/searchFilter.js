f9ba98478cf00ee667b4a7918800566fa7b6ffa3
Tuanhao <tuanhao256@gmail.com>
2020-03-29 23:07:25 -0400

    fix searchFilter function and add unit testing

--- utility/searchFilter.js
+++ utility/searchFilter.js
@@ -1,25 +1,37 @@
+/**
+ * SearcFilter returns search results that has at least 1 filterKeywords in the description or body.
+ * If there is no match, this will return the searchResults in the arguement 
+ * and false in the filterSuccess property.
+ * @param {*} connection Database connection
+ * @param {Object[]} searchResults Array of search results
+ * @param {string[]} filterKeywords Array of keywords
+ * @param {number} userId User ID number
+ * @returns {Object[], boolean} filteredResults: the resulting array; filterSuccess: true if there is any matches of filterKeywords
+ */
 function searchFilter(connection, searchResults, filterKeywords, userId) {
   let filteredResults = [];
   searchResults.forEach(sr => {
-    filterKeywords.forEach(kw => {
+    if(filterKeywords.some(kw => {
       const regKW = new RegExp(kw, 'i')
       if (sr.description.search(regKW) != -1 || sr.body.search(regKW) != -1) {
-        filteredResults.push(sr)
+        return true
       }
-    });
+    })) {
+      filteredResults.push(sr)
+    }
   })
   if (filteredResults.length > 10) {
     // store the new results that is other than the first 10
     // splcie the first ten results then replace all apostrohe (') with ('') because database doesnt like it
-    insert(connection,`REPLACE INTO temp_search_result(userId, searchResults) VALUES('${userId}', '${JSON.stringify(filteredResults.splice(0, 10)).replace(/'/g,"''")}')`)
+    insert(connection,`REPLACE INTO temp_search_result(userId, searchResults) VALUES('${userId}', '${JSON.stringify(filteredResults.slice(10)).replace(/'/g,"''")}')`)
+    return {filteredResults: filteredResults.slice(0, 10), filterSuccess: true}
   }
   if (filteredResults.length == 0) {
     // no matches results, so return general results
-    console.log(searchResults.toString());
-    insert(connection,`REPLACE INTO temp_search_result(userId, searchResults) VALUES('${userId}', '${JSON.stringify(searchResults.slice(0,10)).replace(/'/g,"''")}')`)
+    insert(connection,`REPLACE INTO temp_search_result(userId, searchResults) VALUES('${userId}', '${JSON.stringify(searchResults.slice(10)).replace(/'/g,"''")}')`)
     return {filteredResults: searchResults.slice(0,10), filterSuccess: false}
   }
-  return {filteredResults: filteredResults.slice(0, 10), filterSuccess: true}
+  return {filteredResults: filteredResults, filterSuccess: true}
 }
 
 function insert(connection, SQLquery) {
--- utility/searchFilter.test.js
+++ utility/searchFilter.test.js
@@ -1,28 +1,47 @@
 const searchFilter = require('./searchFilter')
 
 const mockSearchReuslts = [
-  {description: 'abc def'},
-  {description: 'eee aaa'},
-  {description: 'abc def'},
-  {description: 'eee aaa'},
-  {description: 'abc def'},
-  {description: 'eee aaa'},
-  {description: 'abc def'},
-  {description: 'eee aaa'},
-  {description: 'abc def'},
-  {description: 'eee aaa'},
-  {description: 'abc def'},
-  {description: 'eee aaa'},
+  {description: 'abc def', body: 'number 1'},
+  {description: 'eee aaa', body: 'number 2'},
+  {description: 'abc def', body: 'number 3'},
+  {description: 'eee aaa', body: 'number 4'},
+  {description: 'abc def', body: 'number 5'},
+  {description: 'eee aaa', body: 'number 6'},
+  {description: 'abc def', body: 'number 7'},
+  {description: 'eee aaa', body: 'number 8'},
+  {description: 'abc def', body: 'number 9'},
+  {description: 'eee aaa', body: 'number 10'},
+  {description: 'abc def', body: 'number 11'},
+  {description: 'eee aaa', body: 'number 12'},
 ]
-
-const mockFilterKeywords = ['abc', 'def', 'eee', 'aaa']
 
 const mockUserId = 0;
 
 const mockConnection = {query: jest.fn()}
 
 describe('Testing filtering function', () => {
+  beforeEach(() => {
+    mockConnection.query.mockClear();
+  })
   test('more than 10 matches', () => {
-    
+    const mockFilterKeywords = ['abc', 'def', 'eee', 'aaa']
+    const data = searchFilter(mockConnection, mockSearchReuslts, mockFilterKeywords, mockUserId)
+    expect(data.filteredResults.length).toBe(10)
+    expect(data.filterSuccess).toBe(true)
+    expect(mockConnection.query.mock.calls.length).toBe(1);
+  })
+  test('only 6 matches', () => {
+    const mockFilterKeywords = ['abc', 'def']
+    const data = searchFilter(mockConnection, mockSearchReuslts, mockFilterKeywords, mockUserId)
+    expect(data.filteredResults.length).toBe(6)
+    expect(data.filterSuccess).toBe(true)
+    expect(mockConnection.query.mock.calls.length).toBe(0);
+  })
+  test('with 0 match', () => {
+    const mockFilterKeywords = ['haha', 'fail']
+    const data = searchFilter(mockConnection, mockSearchReuslts, mockFilterKeywords, mockUserId)
+    expect(data.filteredResults.length).toBe(10)
+    expect(data.filterSuccess).toBe(false)
+    expect(mockConnection.query.mock.calls.length).toBe(1);
   })
 })
